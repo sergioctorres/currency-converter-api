26e00ae9dc67af1935d5fafbdb94dac8cd0d2dd1
Sérgio Torres <sergio.ctorres@outlook.com>
2025-09-08 01:33:43 -0300

    feat: add OpenTelemetry and HealthChecks

---
+++ src/WebApi/Configuration/ConfigureOptions.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Currency.Configurations;
+using Infrastructure.Security.Configurations;
+
+namespace WebApi.Configuration;
+
+public static class ConfigureOptions
+{
+    public static void AddAppOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<TokenConfiguration>(configuration.GetSection("TokenConfiguration"));
+        services.Configure<CurrencyProviderConfiguration>(configuration.GetSection("CurrencyProviderConfiguration"));
+    }
+}
---
+++ src/WebApi/Configuration/Observability/ConfigureObservability.cs
@@ -0,0 +1,52 @@
+﻿using HealthChecks.UI.Client;
+using Infrastructure.Currency.Configurations;
+using Infrastructure.Security.Configurations;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+namespace WebApi.Configuration.Observability;
+
+public static class ConfigureObservability
+{
+    public static void AddObservability(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOpenTelemetry()
+            .WithTracing(tracing =>
+            {
+                tracing.AddAspNetCoreInstrumentation()
+                       .AddHttpClientInstrumentation();
+            })
+            .WithMetrics(metrics =>
+            {
+                metrics.AddAspNetCoreInstrumentation()
+                       .AddHttpClientInstrumentation()
+                       .AddRuntimeInstrumentation()
+                       .AddConsoleExporter();
+            });
+
+        var currencyApiProvider = configuration.GetSection("CurrencyProviderConfiguration").Get<CurrencyProviderConfiguration>()!;
+
+        services.AddHealthChecks()
+            .AddCheck("self", () => HealthCheckResult.Healthy())
+            .AddUrlGroup(
+                serviceProvider =>
+                {
+                    var currencyApiProvider = serviceProvider.GetRequiredService<IOptions<CurrencyProviderConfiguration>>().Value;
+                    return new Uri($"{currencyApiProvider.Url}/{currencyApiProvider.HealthCheck.Path}");
+                },
+                name: "Currency API"
+            );
+    }
+
+    public static void UseObservability(this WebApplication app)
+    {
+        app.MapHealthChecks("/health", new HealthCheckOptions
+        {
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+    }
+}
